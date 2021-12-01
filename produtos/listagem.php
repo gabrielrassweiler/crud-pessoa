05b8e46411c74b42eea991629f66e19536d5df04
Gabriel Rassweiler <gabriel_rassweiler@hotmail.com>
2021-12-01 19:52:35 -0300

    [ADD] #paginacao - adicionada paginação para todas listagem de todos os cruds

--- produtos/listagem.php
+++ produtos/listagem.php
@@ -1,44 +1,64 @@
 <?php
-    if (isset($_GET['id']))
-        $id = $_GET['id'];
- 
-    try {
-        if (isset($id)) {
-            $stmt = $conn->prepare('SELECT * FROM produtos WHERE id = :id');
-            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        } else {
-            $stmt = $conn->prepare('SELECT * FROM produtos');
-        }
-        $stmt->execute();
- 
-        $result = $stmt->fetchAll();
+if (isset($_GET['id']))
+    $id = $_GET['id'];
+
+try {
+    $quantidade = 5;
+    $pagina = isset($_GET['paginacao']) ? $_GET['paginacao'] : 1;
+
+    if (isset($id)) {
+        $stmt = $conn->prepare('SELECT * FROM produtos WHERE id = :id');
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    } else {
+        $stmt = $conn->prepare('SELECT * FROM produtos');
+    }
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
+
+    $total = count($result);
+    $numero_pagina = ceil($total / $quantidade);
+    $inicio = ($quantidade * $pagina) - $quantidade;
+    $stmt = $conn->prepare("SELECT * FROM produtos limit $inicio, $quantidade");
+    $stmt->execute();
+
+    $result = $stmt->fetchAll();
 ?>
-<table border="1" class="table table-striped">
-<tr>
+    <table border="1" class="table table-striped">
+        <tr>
             <td>Id</td>
             <td>Nome</td>
             <td>Ação</td>
-</tr>
-<?php
-        if ( count($result) ) {
-            foreach($result as $row) {
-                ?>
+        </tr>
+        <?php
+        if (count($result)) {
+            foreach ($result as $row) {
+        ?>
                 <tr>
-                    <td><?=$row['id']?></td>
-                    <td><?=$row['nome']?></td>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['nome'] ?></td>
                     <td>
-                        <a href="?modulo=produtos&pagina=alterar&id=<?=$row['id']?>">Alterar</a> |
-                        <a href="?modulo=produtos&pagina=deletar&id=<?=$row['id']?>">Excluir</a>
+                        <a href="?modulo=produtos&pagina=alterar&id=<?= $row['id'] ?>">Alterar</a> |
+                        <a href="?modulo=produtos&pagina=deletar&id=<?= $row['id'] ?>">Excluir</a>
                     </td>
                 </tr>
-                <?php
+        <?php
             }
         } else {
             echo "Nenhum resultado retornado.";
         }
-?>
-</table>
+        ?>
+    </table>
+    <nav aria-label="">
+        <ul class="pagination">
+            <?php 
+                for ($i = 1; $i <= $numero_pagina; $i++) { ?>
+                    <li class="page-item"><a class="page-link" href="?modulo=produtos&pagina=listagem&paginacao=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+            <?php    }
+            ?>
+        </ul>
+    </nav>
 <?php
-    } catch(PDOException $e) {
-        echo 'ERROR: ' . $e->getMessage();
-    }
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
